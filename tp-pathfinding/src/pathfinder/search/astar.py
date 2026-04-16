2e8882ac7a4418149fbf7df0527dba751cfdcd8d
Franco <francom1@hotmail.com.ar>
2026-04-16 16:05:49 -0300

    ASTAR Completado

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -22,8 +22,25 @@ class AStarSearch:
         reached = {}
         reached[root.state] = root.cost
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontera = PriorityQueueFrontier()
+        nodo = root
+        frontera.add(nodo, nodo.cost + grid.heuristica_manhattan(nodo.state))
 
+        while True:
+            if frontera.is_empty():
+                return NoSolution(reached)
+            
+            nodo = frontera.pop()
+            
+            if grid.objective_test(nodo.state):
+                return Solution(nodo, reached)
+            
+            for action in grid.actions(nodo.state):
+                sucesor = grid.result(nodo.state, action)
+                costo_sucesor = nodo.cost + grid.individual_cost(nodo.state, action)
+                
+                if sucesor not in reached or costo_sucesor < reached[sucesor]:
+                    nodo_sucesor = Node(action, state=sucesor, cost=costo_sucesor, parent=nodo, action=action)
+                    reached[sucesor] = costo_sucesor
+                    frontera.add(nodo_sucesor, costo_sucesor + grid.heuristica_manhattan(sucesor))
         return NoSolution(reached)
